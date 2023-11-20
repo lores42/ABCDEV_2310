e39fd8f6ebb4cb616da147d1f2716437b01b45c9
lores42 <148881509+lores42@users.noreply.github.com>
2023-11-20 16:26:22 +0100

    exo

---
+++ Algo/Java/mon_premier_projet/src/hello_world/App.java
@@ -0,0 +1,36 @@
+package hello_world;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+		 * Exemple
+		 * de
+		 * code
+		 * Java
+		 */
+		//Decleartion d'une variable de type chaine caractère
+		String mavariable;
+		// Declaration de variable de type entier 
+		int nombre;
+		nombre=5;
+		float nombre2;
+		nombre2=2.5f;
+		double nombre3;
+		nombre3=3.25;
+		long nombre4;
+		nombre4=12254;
+		
+		int resultat=3+2;
+		
+		// declaration d'une variable de type boolean
+		
+		boolean vrai_ou_faut;
+		
+		vrai_ou_faut=false; 
+		mavariable = "hello ";
+		
+		System.out.println(mavariable + "world");
+	}
+
+}
