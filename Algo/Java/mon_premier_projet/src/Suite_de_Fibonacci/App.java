e5d07e124285214f33807e40c1c7d6f226a5dab2
lores42 <148881509+lores42@users.noreply.github.com>
2023-11-30 11:05:10 +0100

    test p

--- Algo/Java/mon_premier_projet/src/Suite_de_Fibonacci/App.java
+++ Algo/Java/mon_premier_projet/src/Suite_de_Fibonacci/App.java
@@ -1,10 +1,25 @@
 package Suite_de_Fibonacci;
 
+import java.util.Scanner;
+
 public class App {
-
+/*
+ variable 
+ 	n est entier
+ 	P est un entier 
+ 	R est un entier 
+ début du programme 
+ 	ercire ("saisir le nombre de départ")
+ 	lire:n
+ 	boucle pour ( n+1 , R
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int n;
+		int R;
+		int P;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("saisir un nombre de départ"); 
 	}
 
 }
